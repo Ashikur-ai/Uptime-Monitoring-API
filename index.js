b3b4208755e49cf9cf8dabe792d31ad101f42c6c
ashiku2 <ashikur1602@gmail.com>
2024-07-17 14:26:22 +0600

    Delete file done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = {};
 
 
 // testing file system 
-data.update('test', 'newFile',{'name':'England', 'language':'English'}, (err) => {
+data.delete('test', 'newFile', (err) => {
     console.log(err);
 });
 

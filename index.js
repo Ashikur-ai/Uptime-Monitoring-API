44c4bdf71a6bedd17abe034b8970b99bdf8d41fe
ashiku2 <ashikur1602@gmail.com>
2024-07-17 14:20:36 +0600

    File update done

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = {};
 
 
 // testing file system 
-data.read('test', 'newFile', (err, data) => {
-    console.log(err, data);
+data.update('test', 'newFile',{'name':'England', 'language':'English'}, (err) => {
+    console.log(err);
 });
 
 
